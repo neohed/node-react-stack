a43318fb86c102b8a4f677c3c7fab0bd1e54899d
neohed <35173474+neohed@users.noreply.github.com>
2022-06-29 15:03:03 +0100

    Prisma ORM

---
+++ Part 7, Prisma DB/node-server/prisma/seed.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  // Blitz everything!
+  await prisma.note.deleteMany();
+  await prisma.user.deleteMany();
+
+  const author = await prisma.author.create({
+    data: {
+      name: 'neohed'
+    },
+  });
+
+  await prisma.note.create({
+    data: {
+      title: 'A Note',
+      content: 'Lorem ipsum dolor sit amet',
+      authorId: author.id,
+      lang: 'en',
+      isLive: true,
+      category: '',
+    },
+  });
+
+  console.log(`Database has been seeded. 🌱`);
+}
+
+seed()
+  .then(() => {
+    console.log('Prisma seed function in prisma/seed.ts executed!')
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
